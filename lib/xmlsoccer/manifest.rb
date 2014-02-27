06f3be492e591277d7f094529c8b5be9baa74a94
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-02-27 13:28:19 -0800

    rescue response

--- lib/xmlsoccer/manifest.rb
+++ lib/xmlsoccer/manifest.rb
@@ -7,7 +7,7 @@ module Xmlsoccer
   	  	define_method(method) do |options|
   	  	  response = self.client.call(method, message: options.merge("ApiKey" => self.api_key))
   	  	  response = response.hash[:envelope][:body]["#{method}_response".to_sym]["#{method}_result".to_sym][:xmlsoccer_com]
-  	  	  response.kind_of?(String) ? raise response.to_s : response.first
+  	  	  response.first rescue response.to_s
   	  	end
   	  end
   	end
