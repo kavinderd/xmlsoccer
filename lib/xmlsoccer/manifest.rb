fb6048de25b4631f7e15cf918e2f562fcd0bce3c
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-02-27 13:36:15 -0800

    include module get available methods in manifest

--- lib/xmlsoccer/manifest.rb
+++ lib/xmlsoccer/manifest.rb
@@ -1,16 +1,18 @@
 module Xmlsoccer
 
   module Manifest
-  
-  	def get_availble_methods
-  	  self.client.operations.each do |method|
-  	  	define_method(method) do |options|
-  	  	  response = self.client.call(method, message: options.merge("ApiKey" => self.api_key))
-  	  	  response = response.hash[:envelope][:body]["#{method}_response".to_sym]["#{method}_result".to_sym][:xmlsoccer_com]
-  	  	  response.first rescue response.to_s
-  	  	end
+  	
+  	def self.included(base)
+  	  def get_availble_methods
+  	    self.client.operations.each do |method|
+  	    	define_method(method) do |options|
+  	    	  response = self.client.call(method, message: options.merge("ApiKey" => self.api_key))
+  	    	  response = response.hash[:envelope][:body]["#{method}_response".to_sym]["#{method}_result".to_sym][:xmlsoccer_com]
+  	    	  response.first rescue response.to_s
+  	    	end
+  	    end
   	  end
-  	end
+    end
 
   end
 end
