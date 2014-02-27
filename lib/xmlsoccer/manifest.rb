529181b67cbcd94d29a08d2b3076905fd06fb54a
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-02-27 13:39:39 -0800

    availble to available

--- lib/xmlsoccer/manifest.rb
+++ lib/xmlsoccer/manifest.rb
@@ -2,17 +2,16 @@ module Xmlsoccer
 
   module Manifest
   	
-  	def self.included(base)
-  	  def get_availble_methods
-  	    self.client.operations.each do |method|
-  	    	define_method(method) do |options|
-  	    	  response = self.client.call(method, message: options.merge("ApiKey" => self.api_key))
-  	    	  response = response.hash[:envelope][:body]["#{method}_response".to_sym]["#{method}_result".to_sym][:xmlsoccer_com]
-  	    	  response.first rescue response.to_s
-  	    	end
-  	    end
+  	
+  	def get_availble_methods
+  	  self.client.operations.each do |method|
+  	  	define_method(method) do |options|
+  	  	  response = self.client.call(method, message: options.merge("ApiKey" => self.api_key))
+  	  	  response = response.hash[:envelope][:body]["#{method}_response".to_sym]["#{method}_result".to_sym][:xmlsoccer_com]
+  	  	  response.first rescue response.to_s
+  	  	end
   	  end
-    end
+  	end
 
   end
 end
