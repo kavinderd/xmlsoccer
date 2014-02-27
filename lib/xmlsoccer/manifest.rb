59475f999d902d8b9fc49b4a3e9e6d64dabc7447
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-02-27 13:51:56 -0800

    send define method rather than calling it on the class

--- lib/xmlsoccer/manifest.rb
+++ lib/xmlsoccer/manifest.rb
@@ -5,7 +5,7 @@ module Xmlsoccer
 
   	def get_available_methods
   	  self.client.operations.each do |method|
-  	  	self.class.define_method(method) do |options|
+  	  	self.class.send(:define_method, method) do |options|
   	  	  response = self.client.call(method, message: options.merge("ApiKey" => self.api_key))
   	  	  response = response.hash[:envelope][:body]["#{method}_response".to_sym]["#{method}_result".to_sym][:xmlsoccer_com]
   	  	  response.first rescue response.to_s
