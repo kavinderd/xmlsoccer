d4cb1f1d0a25e174da424aa127520db11691a330
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-02-27 13:43:26 -0800

    add client to attr accessor in Client

--- lib/xmlsoccer/client.rb
+++ lib/xmlsoccer/client.rb
@@ -3,10 +3,11 @@ module Xmlsoccer
   class Client
   	@api_type = Xmlsoccer::Config::DEMO
     @base_url
+    @client
     
     include Manifest
     
-    attr_accessor :base_url, :api_type, :api_key
+    attr_accessor :base_url, :api_type, :api_key, :client
 
     def initialize(options={})
       raise "API Type not valid" unless Xmlsoccer::Config.valid_type?(options[:api_type])
