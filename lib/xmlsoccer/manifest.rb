785c2ac279d59a8ce6151b892b76dfac862ee98e
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-02-28 08:27:32 -0800

    return response and not response.first

--- lib/xmlsoccer/manifest.rb
+++ lib/xmlsoccer/manifest.rb
@@ -9,7 +9,7 @@ module Xmlsoccer
   	  	  response = self.client.call(method, message: options.merge("ApiKey" => self.api_key))
   	  	  response = response.hash[:envelope][:body]["#{method}_response".to_sym]["#{method}_result".to_sym][:xmlsoccer_com]
   	  	  raise response.to_s unless response.respond_to?(:each)
-  	  	  response.first
+  	  	  response
   	  	end
   	  end
   	end
